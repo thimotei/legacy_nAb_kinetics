72bef10e3c3a2d909a1916706be413e145dfd7dc
thimotei <timothywilliamrussell@gmail.com>
2024-04-16 15:41:39 +0100

    Adding scripts to extract minimal data

---
+++ scripts/extract_data/exposures.R
@@ -0,0 +1,17 @@
+#--- Isolate and save exposure data
+
+# Isolate infection data
+dt_inf <- dt_clean[
+  , .(id, inf_num, last_inf_type, last_inf_date)] |>
+  unique()
+
+# Isolate vaccine data
+dt_vax <- dt_clean[
+  !is.na(last_vax_type)][
+    last_vax_type != "others"][
+      , .(id, vax_num, last_vax_type, last_vax_date)][
+        last_vax_date < ymd("2023-10-22")] |>
+  unique()
+
+fwrite(dt_inf, "data/infections.rds")
+fwrite(dt_vax, "data/vaccines.rds")
