ef25f433e194b9ee04592ce509c0152eb1745465
thimotei <timothywilliamrussell@gmail.com>
2024-07-18 11:25:37 +0100

    tweaking individual-level fit plots

--- scripts/figures/supplement/individual_level_fits_new.R
+++ scripts/figures/supplement/individual_level_fits_new.R
@@ -1,20 +1,23 @@
-# Choosing 4 individuals with exposures in all three waves
-id_wave_count <- dt_ind_fits_plot[, .(NumWaves = uniqueN(Wave)), by = id]
-ids_plot <- id_wave_count[NumWaves == 3, .(id = id)][, id]
+#--- This script relies on the individual level trajectories being
+#--- sampled which happens in the Panel B section of the figure_2 script
 
-dt_ind_fits_plot_subset <- dt_ind_fits_plot[
-  id %in% ids_plot][
-    , plot_id := .GRP, by = id][
-      , plot_id := factor(paste0("Individual ", plot_id))]
+# # Choosing 4 individuals with exposures in all three waves
+# id_wave_count <- dt_ind_fits_plot[, .(NumWaves = uniqueN(Wave)), by = id]
+# ids_plot <- id_wave_count[NumWaves == 3, .(id = id)][, id]
+#
+# dt_ind_fits_plot_subset <- dt_ind_fits_plot[
+#   id %in% ids_plot][
+#     , plot_id := .GRP, by = id][
+#       , plot_id := factor(paste0("Individual ", plot_id))]
+#
+# dt_ind_data_plot_subset <- dt_ind_data_plot[
+#   id %in% ids_plot][
+#     , plot_id := .GRP, by = id][
+#       , plot_id := factor(paste0("Individual ", plot_id))]
+#
+# ids <- dt_ind_fits_plot[stan_id %in% sample(unique(stan_id), 10), unique(stan_id)]
 
-dt_ind_data_plot_subset <- dt_ind_data_plot[
-  id %in% ids_plot][
-    , plot_id := .GRP, by = id][
-      , plot_id := factor(paste0("Individual ", plot_id))]
-
-ids <- dt_ind_fits_plot[stan_id %in% sample(unique(stan_id), 10), unique(stan_id)]
-
-dt_ind_fits_plot <- relevel_factors_for_plots(dt_ind_fits_plot)
+# dt_ind_fits_plot <- relevel_factors_for_plots(dt_ind_fits_plot)
 
 # Plotting panel B
 p_ind <- dt_ind_fits_plot[Wave == "XBB wave"] |>
@@ -49,25 +52,26 @@ p_ind <- dt_ind_fits_plot[Wave == "XBB wave"] |>
   theme(legend.position = "right",
         text = element_text(size = 8, family = "Helvetica"),
         strip.background =element_rect(fill="white"),
-        strip.text = element_text(colour = 'black'),
+        strip.text = element_blank(),
         strip.placement = "outside",
-        plot.title = element_text(face = "bold", size = 9)) +
+        plot.title = element_text(face = "bold", size = 9),
+        axis.text.x = element_text(angle = 45, hjust = 1)
+        ) +
   guides(fill = guide_legend(nrow = 2, byrow = TRUE)) +
   scale_shape_manual(values = c(1, 2, 3)) +
   labs(x = "Date",
        y = expression(paste("Titre value (IC"[50], ")")),
-       tag = "B",
        title = "Individual-level fits") +
   scale_colour_manual(values = manual_pal) +
   scale_fill_manual(values = manual_pal) +
   guides(colour = guide_legend(
     override.aes = list(alpha = 1, size = 1))) +
-  facet_wrap(Wave ~ stan_id, scales = "free", ncol = 12)
+  facet_wrap(Wave ~ stan_id, scales = "free", ncol = 18)
 
 ggsave("outputs/figures/figure_ind_xbb.png",
        p_ind,
        width = 20,
-       height = 38.5,
+       height = 28,
        limitsize = FALSE,
        bg = "white")
 
